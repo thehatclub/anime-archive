59d5c4a3f785c9e4e65ad4b0753961c8daecf0f1
thehatclub <tanner.smiley321@gmail.com>
2023-09-19 10:43:44 -0500

    typography

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,13 +2,16 @@ export default function Root() {
   return (
     <div className="h-screen grid place-items-center">
       <div className="container relative">
-        <img src="/logo.png" alt="logo" width={350} className="eye" />
-        <div className="text-center bg-background relative z-10">
+        <img src="/logo.png" alt="logo" width={300} className="eye" />
+        <div className="text-center bg-background relative z-10 p-1">
           <div className="m-3">
-            <h1 className="text-5xl leading-snug">
+            <h1 className="text-3xl md:text-4xl leading-snug">
               WELCOME TO THE
               <br />
-              <span className="text-accent">ANIME ARCHIVE</span>
+              <span className="text-5xl md:text-6xl text-accent">
+                {" "}
+                ANIME ARCHIVE
+              </span>
             </h1>
             <p className="text-lg">
               A place where you can confidently edit your anime needs
